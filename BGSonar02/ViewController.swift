f71c599d6cdc27fa70158b4490fdd968522ca143
Natthanan Gumyan <Natthananebay@gmail.com>
2022-03-02 14:56:43 +0700

    Initial Commit

---
+++ BGSonar02/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BGSonar02
+//
+//  Created by Natthanan Gumyan on 2/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
